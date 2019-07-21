7c485e96e8312ddd606c7489b42a4052ded8f224
sinan.tok <sinan.tok@demirorenteknoloji.com>
2019-07-21 11:28:27 +0300

    loggerUpload

---
+++ Controllers/LogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NetCoreELK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        private ILogger<LogController> Logger { get; set; }
+
+        public LogController(ILogger<LogController> _logger)
+        {
+            this.Logger = _logger;
+        }
+
+        [HttpGet("Info")]
+        public IActionResult Index()
+        {
+            Logger.LogInformation("Somethink went wrong");
+            return null;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace NetCoreELK
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            NLogBuilder.ConfigureNLog("NLog.config");
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseNLog()
+                .UseStartup<Startup>();
+    }
+}
